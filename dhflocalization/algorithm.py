FIX: angle normalization in odom motion model

dhflocalization/algorithm.py
@@ -24,7 +24,7 @@ import re
 %matplotlib widget
 # %%
 map_fn = '/Users/domonkoscsuzdi/Desktop/Research/Localization/code/dhflocalization/resources/tb3_house_true.pgm'
-simu_data_fn = '/Users/domonkoscsuzdi/Desktop/Research/Localization/code/dhflocalization/resources/topicexport_2.json'
+simu_data_fn = '/Users/domonkoscsuzdi/Desktop/Research/Localization/code/dhflocalization/resources/topicexport_10hz.json'
 
 ogm = GridMap.load_grid_map_from_array(
     PgmProcesser.read_pgm(map_fn), 0.05, 10, 10.05)
@@ -38,21 +38,17 @@ ekf = EKF(motion_model, measurement_model)
 edh = EDH(motion_model, measurement_model)
 x_odom, measurement, x_true = RawDataLoader.loadFromJson(simu_data_fn)
 
-particle_num = 500
+particle_num = 100
 
-# dhf_particle_sets = []
 edh_states = []
 ekf_filtered_states = []
 
 # Draw from the prior
-init_particle_mean = [-3, 1, 0]
-init_particle_covar = [[0.2**2, 0, 0], [0, 0.2**2, 0], [0, 0, 0.02**2]]
-# init_particle_set = ParticleSet.init_from_prior(
-#     particle_num, init_particle_mean, init_particle_covar)
+init_particle_mean = [-3.5, 1.5, 0]
+init_particle_covar = [[0.5**2, 0, 0], [0, 0.5**2, 0], [0, 0, 0.05**2]]
 init_state = StateHypothesis.init_from_particle_prior(
     particle_num, init_particle_mean, init_particle_covar)
 edh_states.append(init_state)
-# dhf_particle_sets.append(init_particle_set)
 ekf_filtered_states.append(init_state)
 
 
@@ -69,8 +65,6 @@ for i in range(1, len(x_odom), 1):
     ekf_filtered_states.append(ekf_state)
     edh_states.append(edh_state)
 
-plotter.plot_ground_truths(
-    edh_states, [0, 1], truths_label="Filtered", linestyle="dotted")
 
 plotter.plot_ground_truths(
     ekf_filtered_states, [0, 1], truths_label="Filtered", linestyle="dotted")
@@ -79,11 +73,13 @@ plotter.plot_ground_truths([StateHypothesis(np.array(odom_pose)+np.array([-3, 1,
                            for odom_pose in x_odom], [0, 1], truths_label="Odom", linestyle="--")
 plotter.plot_ground_truths([StateHypothesis(true_pose)
                            for true_pose in x_true], [0, 1], truths_label="True", linestyle="-")
-# %%
-dhf_poses = [
-    dhf_filtered_state.pose for dhf_filtered_state in dhf_filtered_states]
+plotter.plot_tracks(
+    edh_states, [0, 1])
 
-dhf_poses = np.array(dhf_poses).squeeze()[:, :-1]
+edh_poses = [
+    edh_state.pose for edh_state in edh_states]
+
+edh_poses = np.array(edh_poses).squeeze()[:, :-1]
 
 ekf_poses = [
     ekf_filtered_state.pose for ekf_filtered_state in ekf_filtered_states]
@@ -92,11 +88,18 @@ ekf_poses = np.array(ekf_poses).squeeze()[:, :-1]
 
 true_poses = np.array(x_true)[:, :-1]
 
-print(np.sqrt(np.mean(np.linalg.norm(true_poses-dhf_poses, axis=1)**2)))
+print(np.sqrt(np.mean(np.linalg.norm(true_poses-edh_poses, axis=1)**2)))
 print(np.sqrt(np.mean(np.linalg.norm(true_poses-ekf_poses, axis=1)**2)))
 
 
-# ! TODO Szöget normalizálni.
 # %%
 %load_ext snakeviz
 %snakeviz - -new-tab foo()
+
+# %%
+plotter.ax.plot(
+    (edh_states[1].particles[:, 0] - plotter.background_map.left_lower_x) /
+    plotter.background_map.resolution,
+    (edh_states[1].particles[:, 1] - plotter.background_map.left_lower_y) /
+    plotter.background_map.resolution,
+    linestyle='', marker=".", markersize=1, alpha=0.5)

dhflocalization/filters/edh.py
@@ -43,6 +43,6 @@ class EDH:
 
             particle_poses = particle_poses + self.d_lambda * (np.array(
                 [B @ particle_state for particle_state in particle_poses]) + b.T)
-            particle_poses[particle_poses[:, 2] > np.pi, 2] -= 2*np.pi
-            particle_poses[particle_poses[:, 2] < -np.pi, 2] += 2*np.pi
+            # particle_poses[particle_poses[:, 2] > np.pi, 2] -= 2*np.pi
+            # particle_poses[particle_poses[:, 2] < -np.pi, 2] += 2*np.pi
         return StateHypothesis.init_from_particle_poses(particle_poses)

dhflocalization/kinematics/motionmodel.py
@@ -66,32 +66,48 @@ class OdometryMotionModel(MotionModel):
         state_odom_now = control_input[1]
         fi = state.pose[2, 0]
 
-        delta_rot_1 = np.arctan2(
-            state_odom_now[1] - state_odom_prev[1],
-            state_odom_now[0] - state_odom_prev[0]) - state_odom_prev[2]
+        if (np.linalg.norm(state_odom_now[1] - state_odom_prev[1]) < 0.01):
+            delta_rot_1 = 0
+        else:
+            delta_rot_1 = self.calc_angle_diff(np.arctan2(
+                state_odom_now[1] - state_odom_prev[1],
+                state_odom_now[0] - state_odom_prev[0]), state_odom_prev[2])
 
         delta_trans = np.sqrt(
             (state_odom_now[0] - state_odom_prev[0])**2
             + (state_odom_now[1] - state_odom_prev[1])**2)
 
-        delta_rot_2 = state_odom_now[2] - state_odom_prev[2] - delta_rot_1
+        delta_rot_2 = self.calc_angle_diff(
+            state_odom_now[2] - state_odom_prev[2], delta_rot_1)
+
+        # We want to treat backward and forward motion symmetrically for the
+        # noise model to be applied below.  The standard model seems to assume
+        # forward motion.
+        # From https://github.com/ros-planning/navigation/blob/noetic-devel/amcl/src/amcl/sensors/amcl_odom.cpp
+        delta_rot_1_corr = min(abs(self.calc_angle_diff(delta_rot_1, 0)), abs(
+            self.calc_angle_diff(delta_rot_1, np.pi)))
+        delta_rot_2_corr = min(abs(self.calc_angle_diff(delta_rot_2, 0)), abs(
+            self.calc_angle_diff(delta_rot_2, np.pi)))
 
         control_covar = self.calcControlNoiseCovar(
-            delta_rot_1, delta_rot_2, delta_trans)
+            delta_rot_1_corr, delta_rot_2_corr, delta_trans)
 
         if noise:
-            delta_rot_1 -= np.sqrt(control_covar[0, 0]) * np.random.randn()
-            delta_trans -= np.sqrt(control_covar[1, 1]) * np.random.randn()
-            delta_rot_2 -= np.sqrt(control_covar[2, 2]) * np.random.randn()
+            delta_rot_1 = self.calc_angle_diff(
+                delta_rot_1, np.sqrt(control_covar[0, 0]) * np.random.randn())
+            delta_trans = self.calc_angle_diff(
+                delta_trans, np.sqrt(control_covar[1, 1]) * np.random.randn())
+            delta_rot_2 = self.calc_angle_diff(
+                delta_rot_2, np.sqrt(control_covar[2, 2]) * np.random.randn())
 
         prop_pose = state.pose + np.array([[delta_trans*np.cos(fi + delta_rot_1),
-                                            delta_trans *
-                                            np.sin(fi + delta_rot_1),
-                                            delta_rot_1 + delta_rot_2]]).T
-        if prop_pose[2] > np.pi:
-            prop_pose[2] -= 2*np.pi
-        elif prop_pose[2] < -np.pi:
-            prop_pose[2] += 2*np.pi
+                                          delta_trans *
+                                          np.sin(fi + delta_rot_1),
+                                          delta_rot_1 + delta_rot_2]]).T
+        # if prop_pose[2] > np.pi:
+        #     prop_pose[2] -= 2*np.pi
+        # elif prop_pose[2] < -np.pi:
+        #     prop_pose[2] += 2*np.pi
 
         if len(state.covar):
             jacobi_state, jacobi_input = self.calcJacobians(
@@ -143,3 +159,18 @@ class OdometryMotionModel(MotionModel):
             abs(delta_rot_2)**2 + self.alfa_2 * delta_trans**2
 
         return np.diag([control_covar_11, control_covar_22, control_covar_33])
+
+    def normalize_angle(self, angle):
+        return np.arctan2(np.sin(angle), np.cos(angle))
+
+    def calc_angle_diff(self, a, b):
+        a = self.normalize_angle(a)
+        b = self.normalize_angle(b)
+        d1 = a-b
+        d2 = 2*np.pi - abs(d1)
+        if(d1 > 0):
+            d2 *= -1.0
+        if(abs(d1) < abs(d2)):
+            return d1
+        else:
+            return d2