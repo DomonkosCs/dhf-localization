521c70530f4996b2552f629c7bb791d23560701a
Domonkos Csuzdi <csuzdi02@gmail.com>
2022-02-09 14:04:52 +0100

    change: formatted with black

--- dhflocalization/gridmap/grid_map.py
+++ dhflocalization/gridmap/grid_map.py
@@ -17,8 +17,7 @@ class GridMap:
     GridMap class
     """
 
-    def __init__(self, width, height, resolution,
-                 center_x, center_y, init_val=0.0):
+    def __init__(self, width, height, resolution, center_x, center_y, init_val=0.0):
         """__init__
         :param width: number of grid for width
         :param height: number of grid for heigt
@@ -33,8 +32,8 @@ class GridMap:
         self.center_x = center_x
         self.center_y = center_y
 
-        #self.left_lower_x = self.center_x - self.width / 2.0 * self.resolution
-        #self.left_lower_y = self.center_y - self.height / 2.0 * self.resolution
+        # self.left_lower_x = self.center_x - self.width / 2.0 * self.resolution
+        # self.left_lower_y = self.center_y - self.height / 2.0 * self.resolution
         self.left_lower_x = -self.center_x
         self.left_lower_y = -self.center_y
 
@@ -47,10 +46,9 @@ class GridMap:
     @classmethod
     def load_grid_map_from_csv(cls, csv_fn, resolution, center_x, center_y):
         path = pkg_resources.resource_filename(
-            __name__,
-            os.path.join(os.pardir, 'resources', csv_fn)
+            __name__, os.path.join(os.pardir, "resources", csv_fn)
         )
-        map = np.genfromtxt(path, delimiter=';', dtype=np.float32)
+        map = np.genfromtxt(path, delimiter=";", dtype=np.float32)
         if np.NaN in map:
             return None
         height = map.shape[0]
@@ -59,20 +57,23 @@ class GridMap:
         center_x = center_x
         center_y = center_y
 
-        gm = cls(height=height,
-                 width=width,
-                 resolution=resolution,
-                 center_x=center_x,
-                 center_y=center_y,
-                 )
+        gm = cls(
+            height=height,
+            width=width,
+            resolution=resolution,
+            center_x=center_x,
+            center_y=center_y,
+        )
 
         for idx, val in np.ndenumerate(map):
-            cls.set_value_from_xy_index(
-                self=gm, x_ind=idx[0], y_ind=idx[1], val=val)
+            cls.set_value_from_xy_index(self=gm, x_ind=idx[0], y_ind=idx[1], val=val)
 
         gm.distance_transform = cls.calc_distance_transform(gm)
         gm.distance_transform_interp = RectBivariateSpline(
-            np.arange(width)*resolution, np.arange(height)*resolution, gm.distance_transform*resolution)
+            np.arange(width) * resolution,
+            np.arange(height) * resolution,
+            gm.distance_transform * resolution,
+        )
         return gm
 
     @classmethod
@@ -80,19 +81,25 @@ class GridMap:
         height = map.shape[0]
         width = map.shape[1]
 
-        gm = cls(height=height,
-                 width=width,
-                 resolution=resolution,
-                 center_x=center_x,
-                 center_y=center_y)
+        gm = cls(
+            height=height,
+            width=width,
+            resolution=resolution,
+            center_x=center_x,
+            center_y=center_y,
+        )
 
         for idx, val in np.ndenumerate(map):
             cls.set_value_from_xy_index(
-                self=gm, x_ind=idx[1], y_ind=height-idx[0], val=val)
+                self=gm, x_ind=idx[1], y_ind=height - idx[0], val=val
+            )
 
         gm.distance_transform = cls.calc_distance_transform(gm)
         gm.distance_transform_interp = RectBivariateSpline(
-            np.arange(width)*resolution, np.arange(height)*resolution, gm.distance_transform*resolution)
+            np.arange(width) * resolution,
+            np.arange(height) * resolution,
+            gm.distance_transform * resolution,
+        )
         return gm
 
     def get_value_from_xy_index(self, x_ind, y_ind):
@@ -114,10 +121,8 @@ class GridMap:
         :param x_pos: x position [m]
         :param y_pos: y position [m]
         """
-        x_ind = self.calc_xy_index_from_position(
-            x_pos, self.left_lower_x, self.width)
-        y_ind = self.calc_xy_index_from_position(
-            y_pos, self.left_lower_y, self.height)
+        x_ind = self.calc_xy_index_from_position(x_pos, self.left_lower_x, self.width)
+        y_ind = self.calc_xy_index_from_position(y_pos, self.left_lower_y, self.height)
 
         return x_ind, y_ind
 
@@ -162,10 +167,8 @@ class GridMap:
         return grid_ind
 
     def calc_grid_central_xy_position_from_xy_index(self, x_ind, y_ind):
-        x_pos = self.calc_grid_central_xy_position_from_index(
-            x_ind, self.left_lower_x)
-        y_pos = self.calc_grid_central_xy_position_from_index(
-            y_ind, self.left_lower_y)
+        x_pos = self.calc_grid_central_xy_position_from_index(x_ind, self.left_lower_x)
+        y_pos = self.calc_grid_central_xy_position_from_index(y_ind, self.left_lower_y)
 
         return x_pos, y_pos
 
@@ -192,12 +195,19 @@ class GridMap:
         edt_interp = self.distance_transform_interp
 
         # zero at the middle of the cell
-        return edt_interp.ev(y_pos-self.left_lower_y-self.resolution/2.0, x_pos-self.left_lower_x-self.resolution/2.0)
+        return edt_interp.ev(
+            y_pos - self.left_lower_y - self.resolution / 2.0,
+            x_pos - self.left_lower_x - self.resolution / 2.0,
+        )
 
     def calc_distance_function_derivate_interp(self, x_pos, y_pos, dx, dy):
-        edt = self.distance_transform
         edt_interp = self.distance_transform_interp
-        return edt_interp.ev(y_pos-self.left_lower_y-self.resolution/2.0, x_pos-self.left_lower_x-self.resolution/2.0, dy, dx)
+        return edt_interp.ev(
+            y_pos - self.left_lower_y - self.resolution / 2.0,
+            x_pos - self.left_lower_x - self.resolution / 2.0,
+            dy,
+            dx,
+        )
 
     def check_occupied_from_xy_index(self, xind, yind, occupied_val=1.0):
 
@@ -240,8 +250,9 @@ class GridMap:
         grid_data = np.reshape(np.array(self.data), (self.height, self.width))
         if not ax:
             fig, ax = plt.subplots()
-        heat_map = ax.imshow(grid_data, cmap="Greys",
-                             vmin=0.0, vmax=1.0, zorder=zorder, origin="lower")
+        heat_map = ax.imshow(
+            grid_data, cmap="Greys", vmin=0.0, vmax=1.0, zorder=zorder, origin="lower"
+        )
         plt.axis("equal")
         plt.grid()
         return heat_map
@@ -256,14 +267,13 @@ class GridMap:
         bottom = np.zeros_like(edt)
         width = depth = 1
 
-        ax = fig.add_subplot(121, projection='3d')
+        ax = fig.add_subplot(121, projection="3d")
 
-        cmap = cm.get_cmap('jet')
+        cmap = cm.get_cmap("jet")
         print(edt.min(), edt.max())
-        rgba = [cmap((bar-edt.min())/edt.max()) for bar in edt.ravel()]
+        rgba = [cmap((bar - edt.min()) / edt.max()) for bar in edt.ravel()]
 
-        ax.bar3d(x, y, bottom, width, depth,
-                 edt.ravel(), color=rgba, shade=True)
+        ax.bar3d(x, y, bottom, width, depth, edt.ravel(), color=rgba, shade=True)
 
         return ax
 
@@ -273,8 +283,11 @@ class GridMap:
         _y = np.linspace(0, self.height, 101)
         _xx, _yy = np.meshgrid(_x, _y)
 
-        z = [self.calc_distance_transform_xy_pos(x, y)[0][0]
-             for x, y in zip(_xx.ravel(), _yy.ravel())]
-        ax = fig.add_subplot(122, projection='3d')
-        ax.plot_surface(_xx, _yy, np.array(z).reshape(_xx.shape),
-                        cmap='jet', edgecolor='none')
+        z = [
+            self.calc_distance_transform_xy_pos(x, y)[0][0]
+            for x, y in zip(_xx.ravel(), _yy.ravel())
+        ]
+        ax = fig.add_subplot(122, projection="3d")
+        ax.plot_surface(
+            _xx, _yy, np.array(z).reshape(_xx.shape), cmap="jet", edgecolor="none"
+        )
--- dhflocalization/visualization/plotter.py
+++ dhflocalization/visualization/plotter.py
@@ -1,7 +1,5 @@
 # From Stone Soup: https://github.com/dstl/Stone-Soup
 
-import warnings
-from itertools import chain
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -35,14 +33,16 @@ class Plotter:
         self.ax = self.fig.add_subplot(1, 1, 1)
         self.ax.set_xlabel("$x$")
         self.ax.set_ylabel("$y$")
-        self.ax.axis('equal')
+        self.ax.axis("equal")
         self.background_map = None
 
         # Create empty lists for legend handles and labels
         self.handles_list = []
         self.labels_list = []
 
-    def plot_ground_truths(self, truths, mapping, truths_label="Ground Truth", **kwargs):
+    def plot_ground_truths(
+        self, truths, mapping, truths_label="Ground Truth", **kwargs
+    ):
         """Plots ground truth(s)
 
         Plots each ground truth path passed in to :attr:`truths` and generates a legend
@@ -71,32 +71,42 @@ class Plotter:
         if self.background_map is not None:
             self.background_map.plot_grid_map(ax=self.ax)
             for truth in truths:
-                x_coords = [(state.pose[mapping[0], 0] - self.background_map.left_lower_x) /
-                            self.background_map.resolution for state in truth]
-                y_coords = [(state.pose[mapping[1], 0] - self.background_map.left_lower_y)/self.background_map.resolution
-                            for state in truth]
-                self.ax.plot(x_coords,
-                             y_coords,
-                             **truths_kwargs, zorder=2)
+                x_coords = [
+                    (state.pose[mapping[0], 0] - self.background_map.left_lower_x)
+                    / self.background_map.resolution
+                    for state in truth
+                ]
+                y_coords = [
+                    (state.pose[mapping[1], 0] - self.background_map.left_lower_y)
+                    / self.background_map.resolution
+                    for state in truth
+                ]
+                self.ax.plot(x_coords, y_coords, **truths_kwargs, zorder=2)
         else:
             for truth in truths:
                 x_coords = [state.pose[mapping[0], 0] for state in truth]
                 y_coords = [state.pose[mapping[1], 0] for state in truth]
-                self.ax.plot(x_coords,
-                             y_coords,
-                             **truths_kwargs, zorder=2)
+                self.ax.plot(x_coords, y_coords, **truths_kwargs, zorder=2)
 
         # Generate legend items
         truths_handle = Line2D(
-            [], [], linestyle=truths_kwargs['linestyle'], color='black')
+            [], [], linestyle=truths_kwargs["linestyle"], color="black"
+        )
         self.handles_list.append(truths_handle)
         self.labels_list.append(truths_label)
 
         # Generate legend
         self.ax.legend(handles=self.handles_list, labels=self.labels_list)
 
-    def plot_tracks(self, tracks, mapping, uncertainty=False, particle=False, track_label="Track",
-                    **kwargs):
+    def plot_tracks(
+        self,
+        tracks,
+        mapping,
+        uncertainty=False,
+        particle=False,
+        track_label="Track",
+        **kwargs
+    ):
         """Plots track(s)
 
         Plots each track generated, generating a legend automatically. If ``uncertainty=True``,
@@ -125,7 +135,7 @@ class Plotter:
             ``marker='.'`` and ``color=None``.
         """
 
-        tracks_kwargs = dict(linestyle='-', marker=".", color=None)
+        tracks_kwargs = dict(linestyle="-", marker=".", color=None)
         tracks_kwargs.update(kwargs)
 
         if not isinstance(tracks, set):
@@ -134,20 +144,33 @@ class Plotter:
         # Plot tracks
         track_colors = {}
         for track in tracks:
-            line = self.ax.plot([(state.pose[mapping[0], 0] - self.background_map.left_lower_x) /
-                                self.background_map.resolution for state in track],
-                                [(state.pose[mapping[1], 0] - self.background_map.left_lower_y)/self.background_map.resolution
-                                for state in track],
-                                **tracks_kwargs)
-            track_colors[track] = plt.getp(line[0], 'color')
+            line = self.ax.plot(
+                [
+                    (state.pose[mapping[0], 0] - self.background_map.left_lower_x)
+                    / self.background_map.resolution
+                    for state in track
+                ],
+                [
+                    (state.pose[mapping[1], 0] - self.background_map.left_lower_y)
+                    / self.background_map.resolution
+                    for state in track
+                ],
+                **tracks_kwargs
+            )
+            track_colors[track] = plt.getp(line[0], "color")
 
         # Assuming a single track or all plotted as the same colour then the following will work.
         # Otherwise will just render the final track colour.
-        tracks_kwargs['color'] = plt.getp(line[0], 'color')
+        tracks_kwargs["color"] = plt.getp(line[0], "color")
 
         # Generate legend items for track
-        track_handle = Line2D([], [], linestyle=tracks_kwargs['linestyle'],
-                              marker=tracks_kwargs['marker'], color=tracks_kwargs['color'])
+        track_handle = Line2D(
+            [],
+            [],
+            linestyle=tracks_kwargs["linestyle"],
+            marker=tracks_kwargs["marker"],
+            color=tracks_kwargs["color"],
+        )
         self.handles_list.append(track_handle)
         self.labels_list.append(track_label)
 
@@ -161,30 +184,37 @@ class Plotter:
                     max_ind = np.argmax(w)
                     min_ind = np.argmin(w)
                     orient = np.arctan2(v[1, max_ind], v[0, max_ind])
-                    coords = ((state.pose[mapping[0], 0] - self.background_map.left_lower_x) /
-                              self.background_map.resolution, (state.pose[mapping[1], 0] - self.background_map.left_lower_y)/self.background_map.resolution)
-                    ellipse = Ellipse(xy=coords,
-                                      width=2 *
-                                      np.sqrt(w[max_ind]) /
-                                      self.background_map.resolution,
-                                      height=2 *
-                                      np.sqrt(w[min_ind]) /
-                                      self.background_map.resolution,
-                                      angle=np.rad2deg(orient), alpha=0.2,
-                                      color=track_colors[track])
+                    coords = (
+                        (state.pose[mapping[0], 0] - self.background_map.left_lower_x)
+                        / self.background_map.resolution,
+                        (state.pose[mapping[1], 0] - self.background_map.left_lower_y)
+                        / self.background_map.resolution,
+                    )
+                    ellipse = Ellipse(
+                        xy=coords,
+                        width=2 * np.sqrt(w[max_ind]) / self.background_map.resolution,
+                        height=2 * np.sqrt(w[min_ind]) / self.background_map.resolution,
+                        angle=np.rad2deg(orient),
+                        alpha=0.2,
+                        color=track_colors[track],
+                    )
                     self.ax.add_artist(ellipse)
 
             # Generate legend items for uncertainty ellipses
             ellipse_handle = Ellipse(
-                (0.5, 0.5), 0.5, 0.5, alpha=0.2, color=tracks_kwargs['color'])
+                (0.5, 0.5), 0.5, 0.5, alpha=0.2, color=tracks_kwargs["color"]
+            )
             ellipse_label = "Uncertainty"
 
             self.handles_list.append(ellipse_handle)
             self.labels_list.append(ellipse_label)
 
             # Generate legend
-            self.ax.legend(handles=self.handles_list, labels=self.labels_list,
-                           handler_map={Ellipse: _HandlerEllipse()})
+            self.ax.legend(
+                handles=self.handles_list,
+                labels=self.labels_list,
+                handler_map={Ellipse: _HandlerEllipse()},
+            )
 
         elif particle:
             # Plot particles
@@ -192,15 +222,20 @@ class Plotter:
                 for state in track:
                     data = state.particles[:, mapping[:2]]
                     self.ax.plot(
-                        (data[:, 0] - self.background_map.left_lower_x) /
-                        self.background_map.resolution,
-                        (data[:, 1] - self.background_map.left_lower_y) /
-                        self.background_map.resolution,
-                        linestyle='', marker=".", markersize=1, alpha=0.5)
+                        (data[:, 0] - self.background_map.left_lower_x)
+                        / self.background_map.resolution,
+                        (data[:, 1] - self.background_map.left_lower_y)
+                        / self.background_map.resolution,
+                        linestyle="",
+                        marker=".",
+                        markersize=1,
+                        alpha=0.5,
+                    )
 
             # Generate legend items for particles
             particle_handle = Line2D(
-                [], [], linestyle='', color="black", marker='.', markersize=1)
+                [], [], linestyle="", color="black", marker=".", markersize=1
+            )
             particle_label = "Particles"
             self.handles_list.append(particle_handle)
             self.labels_list.append(particle_label)
@@ -212,7 +247,7 @@ class Plotter:
             self.ax.legend(handles=self.handles_list, labels=self.labels_list)
 
     # Ellipse legend patch (used in Tutorial 3)
-    @ staticmethod
+    @staticmethod
     def ellipse_legend(ax, label_list, color_list, **kwargs):
         """Adds an ellipse patch to the legend on the axes. One patch added for each item in
         `label_list` with the corresponding color from `color_list`.
@@ -236,8 +271,7 @@ class Plotter:
         legend = ax.legend(handler_map={Ellipse: _HandlerEllipse()})
         handles, labels = ax.get_legend_handles_labels()
         for color in color_list:
-            handle = Ellipse((0.5, 0.5), 0.5, 0.5,
-                             color=color, **ellipse_kwargs)
+            handle = Ellipse((0.5, 0.5), 0.5, 0.5, color=color, **ellipse_kwargs)
             handles.append(handle)
         for label in label_list:
             labels.append(label)
@@ -248,11 +282,11 @@ class Plotter:
 
 
 class _HandlerEllipse(HandlerPatch):
-    def create_artists(self, legend, orig_handle,
-                       xdescent, ydescent, width, height, fontsize, trans):
-        center = 0.5*width - 0.5*xdescent, 0.5*height - 0.5*ydescent
-        p = Ellipse(xy=center, width=width + xdescent,
-                    height=height + ydescent)
+    def create_artists(
+        self, legend, orig_handle, xdescent, ydescent, width, height, fontsize, trans
+    ):
+        center = 0.5 * width - 0.5 * xdescent, 0.5 * height - 0.5 * ydescent
+        p = Ellipse(xy=center, width=width + xdescent, height=height + ydescent)
         self.update_prop(p, orig_handle, legend)
         p.set_transform(trans)
         return [p]
