fix: metrics calculation when comptime is recorded

dhflocalization/evaluator/metrics.py
@@ -26,77 +26,55 @@ def _angle_set_diff(set_1, set_2):
 def _calc_error_metrics(filtered_poses, reference_poses):
 
     err_mean_sqare = {"position": {}, "orientation": {}}
-    for key, value in filtered_poses.items():
-        value = np.array(value)
-        err_mean_sqare["position"][key] = float(
-            np.sqrt(
-                np.mean(
-                    np.linalg.norm(
-                        reference_poses["true"][:, :-1] - value[:, :-1], axis=1
-                    )
-                    ** 2
-                )
-            )
-        )
-        err_mean_sqare["orientation"][key] = float(
-            np.sqrt(
-                np.mean(
-                    np.array(
-                        _angle_set_diff(reference_poses["true"][:, 2], value[:, 2])
-                    )
-                    ** 2
-                )
-            )
-        )
-
     err_mean_abs = {"position": {}, "orientation": {}}
-    for key, value in filtered_poses.items():
-        err_mean_abs["position"][key] = float(
-            np.mean(
-                np.linalg.norm(reference_poses["true"][:, :-1] - value[:, :-1], axis=1)
-            )
-        )
-        err_mean_abs["orientation"][key] = float(
-            np.mean(
-                np.abs(
-                    np.array(
-                        _angle_set_diff(reference_poses["true"][:, 2], value[:, 2])
-                    )
-                )
-            )
-        )
+    err_std = {"position": {}, "orientation": {}}
 
-    std = {"position": {}, "orientation": {}}
-    for key, value in filtered_poses.items():
-        std["position"][key] = float(
-            np.std(
-                np.linalg.norm(reference_poses["true"][:, :-1] - value[:, :-1], axis=1)
-            )
-        )
-        std["orientation"][key] = float(
-            np.std(
-                np.array(_angle_set_diff(reference_poses["true"][:, 2], value[:, 2]))
-            )
-        )
+    for algo, value in filtered_poses.items():
+        states = np.array(value["state"])
 
-    return err_mean_sqare, err_mean_abs, std
+        true_xy = reference_poses["true"][:, :-1]
+        true_angle = reference_poses["true"][:, 2]
+
+        filtered_xy = states[:, :-1]
+        filtered_angle = states[:, 2]
+
+        err_xy_norm = np.linalg.norm(true_xy - filtered_xy, axis=1)
+        err_angle = np.array(_angle_set_diff(true_angle, filtered_angle))
+
+        # MSE
+        err_mean_sqare["position"][algo] = float(np.sqrt(np.mean(err_xy_norm ** 2)))
+        err_mean_sqare["orientation"][algo] = float(np.sqrt(np.mean(err_angle**2)))
+
+        # MAE
+        err_mean_abs["position"][algo] = float(np.mean(err_xy_norm))
+        err_mean_abs["orientation"][algo] = float(np.mean(np.abs(err_angle)))
+
+        # STD of error
+        err_std["position"][algo] = float(np.std(err_xy_norm))
+        err_std["orientation"][algo] = float(np.std(err_angle))
+
+    return err_mean_sqare, err_mean_abs, err_std
 
 
 def eval(
-    filtered_states, reference_states, export_filename=None, return_results=False,
+    filtered_states,
+    reference_states,
+    export_filename=None,
+    return_results=False,
 ):
 
-    err_mean_sqare, err_mean_abs, std = _calc_error_metrics(
+    err_mean_sqare, err_mean_abs, err_std = _calc_error_metrics(
         filtered_states, reference_states
     )
     if export_filename:
         metrics_dict = {
             "RMSE": err_mean_sqare,
             "MAE": err_mean_abs,
-            "STD": std,
+            "STD": err_std,
         }
         YamlWriter().updateFile(
-            payload=metrics_dict, filename=export_filename,
+            payload=metrics_dict,
+            filename=export_filename,
         )
     if return_results:
-        return err_mean_sqare, err_mean_abs, std
+        return err_mean_sqare, err_mean_abs, err_std