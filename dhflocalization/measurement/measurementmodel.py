46fdc46d32d2f8b9c4007cfb028f0d7d61f3aa3f
Domonkos Csuzdi <csuzdi02@gmail.com>
2023-03-29 14:51:16 +0200

    fix: transform between robot and laser frame

--- dhflocalization/measurement/measurementmodel.py
+++ dhflocalization/measurement/measurementmodel.py
@@ -9,12 +9,11 @@ class MeasurementModel:
         range_noise_std,
         robot_sensor_dx=0,
         robot_sensor_dy=0,
-        robot_sensor_dyaw=0,
     ):
         self.ogm = ogm
         self.range_noise_std = range_noise_std
         self.robot_sensor_tr = np.array(
-            [robot_sensor_dx, robot_sensor_dy, robot_sensor_dyaw]
+            [robot_sensor_dx, robot_sensor_dy]
         )
 
     def process_detection(self, state_vector, measurement):
@@ -22,16 +21,18 @@ class MeasurementModel:
         ranges = [ray[1] for ray in measurement]
         angles = [ray[0] for ray in measurement]
 
+
+
         x_o = np.zeros([len(ranges), 2])
         ogm = self.ogm
 
         # Transform readings from sensor frame to global frame.
         # First, transform to the robot's frame, and then to the global.
-        angle_global = angles + self.robot_sensor_tr[2] + state_vector[2]
+        angle_global = angles + state_vector[2]
         r_cos = np.multiply(ranges, np.cos(angle_global))
         r_sin = np.multiply(ranges, np.sin(angle_global))
-        x_o[:, 0] = r_cos + self.robot_sensor_tr[0] + state_vector[0]
-        x_o[:, 1] = r_sin + self.robot_sensor_tr[1] + state_vector[1]
+        x_o[:, 0] = r_cos + np.cos(state_vector[2]) * self.robot_sensor_tr[0] - np.sin(state_vector[2]) * self.robot_sensor_tr[1] + state_vector[0]
+        x_o[:, 1] = r_sin + np.sin(state_vector[2]) * self.robot_sensor_tr[0] + np.cos(state_vector[2]) * self.robot_sensor_tr[1] + state_vector[1]
 
         df = ogm.calc_distance_transform_xy_pos(x_o)
 
