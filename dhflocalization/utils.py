f65b6ce56b64996672fa2fdc3856a5d833e1a737
Domonkos Csuzdi <csuzdi02@gmail.com>
2023-04-04 13:01:58 +0200

    refactor: move angle normalization to utils

---
+++ dhflocalization/utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def normalize_angle(angle):
+    return np.arctan2(np.sin(angle), np.cos(angle))
+
+
+def calc_angle_diff(a, b):
+    a = normalize_angle(a)
+    b = normalize_angle(b)
+    d1 = a - b
+    d2 = 2 * np.pi - abs(d1)
+    if d1 > 0:
+        d2 *= -1.0
+    if abs(d1) < abs(d2):
+        return d1
+    else:
+        return d2
+
+def angle_set_diff(set_1, set_2):
+    return [calc_angle_diff(a, b) for a, b in zip(set_1, set_2)]
