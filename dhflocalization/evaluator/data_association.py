b2c70e7a25267d9fce45e10e590abd0bc197d02d
Domonkos Csuzdi <csuzdi02@gmail.com>
2023-06-28 13:41:26 +0200

    remove unnecessary files

--- dhflocalization/evaluator/data_association.py
+++
@@ -1,9 +0,0 @@
-from bisect import bisect_left
-
-
-def optitrack_with_filter(optitrack_stamps, filter_stamps):
-    closest_indices = list(
-        map(lambda i: bisect_left(optitrack_stamps, i), filter_stamps)
-    )
-
-    return closest_indices
